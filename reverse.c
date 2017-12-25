10000c7dbe99b6be218d6cfc367f8c89baa82e54
bobsira <sbobfitz2@gmail.com>
2017-12-25 23:21:00 +0300

    Reverse an Integer

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  /* code */
+  int n, reversedNumber = 0 , remainder;
+
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+
+  while (n!= 0) {
+    remainder = n%10;
+    reversedNumber = reversedNumber*10 + remainder;
+    n /= 10;
+  }
+
+  printf("Reversed Number = %d\n", reversedNumber);
+  return 0;
+}
