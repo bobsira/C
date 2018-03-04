fb6360bc8cdc1152bd90e1b50480c2b76c74b302
bobsira <sbobfitz2@gmail.com>
2018-03-04 22:20:47 +0300

    Using getchar() to read a line of text

---
+++ practise/read_line_oftext_characterbycharacter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  char name[30], ch;
+  int i = 0;
+  printf("Enter name: ");
+  while (ch != '\n') {  // terminate if user hit enter
+    ch = getchar();
+    name[i] = ch;
+    i++;
+  }
+  name[i] = '\0';  // inserting null character at end
+  printf("Name: %s", name);
+  return 0;
+}
