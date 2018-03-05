fd245d85dec3f54ee2cdc6d8e7088ff36066e19e
bobsira <sbobfitz2@gmail.com>
2018-03-05 20:38:36 +0300

    Two-Dimensional Arrays

---
+++ book/chapter4/twoD.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+
+  int t, i, num[3][4];
+
+  for(t=0;t<3;t++)
+    for(i=0;i<4;i++)
+      num[t][i] = (t*4)+i+1;
+
+  for(t=0;t<3;t++){
+    for(i=0;i<4;i++)
+      printf("%3d ",num[t][i]);
+    printf("\n");
+  }
+  return 0;
+}
