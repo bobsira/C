2eb82739ac3b8cfbc322e4fdcac10fbb62fe8878
bobsira <sbobfitz2@gmail.com>
2018-03-09 14:32:19 +0300

    Unsized Array Initializations

--- book/chapter4/arrays_initialization.c
+++ book/chapter4/arrays_initialization.c
@@ -39,8 +39,24 @@ int main(int argc, char const *argv[]) {
   char e2[13] = "Write error\n";
   char e3[18] = "Cannot open file\n";
 
-  /**/
+  /* same as above - unsized array */
   char e1[] = "Read error\n";
   char e2[] = "Write error\n";
   char e3[] = "Cannot open file\n";
-  printf("%s has length %d\n", e2, sizeof e2);
+  printf("%s has length %d\n", e2, (int)sizeof e2);
+
+  /* unsized two dimension array */
+  int sqrs[][2] = {
+    {1, 1},
+    {2, 4},
+    {3, 9},
+    {4, 16},
+    {5, 25},
+    {6, 36},
+    {7, 49},
+    {8, 64},
+    {9, 81},
+    {10, 100}
+  };
+  return 0;
+}
