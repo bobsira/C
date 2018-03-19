d41024b6b5b5de18b8b785765e1c0288c0888e68
bobsira <sbobfitz2@gmail.com>
2018-03-20 00:23:56 +0300

    Pointers to functions

---
+++ book/chapter5/pointers_to_functions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+void check(char *a, char *b, int (*cmp)(const char *, const char *));
+int numcmp(const char *a, const char *b);
+
+int main(int argc, char const *argv[]) {
+  char s1[80], s2[80];
+  int (*p)(const char *, const char *);
+  p = strcmp;
+  //gets(s1);
+  scanf ("%[^\n]%*c", s1);
+  //gets(s2);
+  scanf ("%[^\n]%*c", s2);
+  check(s1, s2, p); //check(s1, s2, strcmp);
+  return 0;
+}
+
+void check(char *a, char *b, int (*cmp)(const char *, const char *)){
+  printf("Testing for equality.\n");
+  if(!(*cmp)(a, b)) printf("Equal\n");  //cmp(a, b);
+  else printf("Not Equal\n");
+}
