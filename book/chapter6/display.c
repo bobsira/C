6c9a47a9157ed9201e34db688861d96e5ae24aa5
bobsira <sbobfitz2@gmail.com>
2018-04-08 00:44:02 +0300

    argc and argv—Arguments to main( )

---
+++ book/chapter6/display.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char *argv[]){
+  int t, i;
+  for(t=0; t<argc; ++t) {
+    i = 0;
+    while(argv[t][i]) {
+      putchar(argv[t][i]);
+      ++i;
+    }
+    printf("\n");
+  }
+  return 0;
+}
--- gradeavg.c
+++ gradeavg.c
@@ -5,8 +5,8 @@ int main(int argc, char const *argv[]) {
   int marksTotal = 0;
   int numberOfStudentsEntered = 0;
   float average;
-  printf("Enter first person's marks or -1 to quit\n");
-  scanf("%d",&marks);
+  // printf("Enter first person's marks or -1 to quit\n");
+  // scanf("%d",&marks);
   while (marks!=-1) {
     marksTotal = marksTotal + marks;
     numberOfStudentsEntered++;
